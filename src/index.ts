8d8323d190b9cad15b081159183e925b4cf66d73
johnfedoruk <johnny@johnfedoruk.ca>
2018-03-31 22:43:20 -0500

    Execute a NewProjectCommand on --new

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import * as program from "commander";
 import { LoggingService as Log } from './services/logging/logging.service';
+import { Command } from "./commands/command";
+import { NewProjectCommand } from "./commands/new-project/new-project.command";
+import { NewProjectOptions } from "./models/command-options/new-project-options/new-project-options";
+import { CommandOptions } from "./models/command-options/command-options";
+
 
 export const main = function (...args: string[]): number {
+    let cmd: Command;
+    let opts: CommandOptions;
     Log.log(`
     __ _____  _    _   _    __  _  _  _  _ _ _ 
     \\ V / __|| |  | | / \\  / _|| \\| |/ \\| | | |
@@ -12,6 +19,13 @@ export const main = function (...args: string[]): number {
         .version('0.1.0')
         .option('-n, --new [name]', 'Generate a new project', 'hello-world')
         .parse(process.argv);
-    if (program.new) Log.log(`Generating a new project "${program.new}"`);
+    
+    if (program.new) {
+        const name: string = program.new;
+        opts = new NewProjectOptions(name);
+        cmd = new NewProjectCommand();
+        cmd.run(opts);
+    }
+
     return 0;
 }
