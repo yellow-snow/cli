2b0ef7ae278cf53171f5ddef3ae74ee4b80b9639
johnfedoruk <johnny@johnfedoruk.ca>
2018-03-31 23:01:15 -0500

    Added HelpCommand to index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as program from "commander";
 import { LoggingService as Log } from './services/logging/logging.service';
 import { Command } from "./commands/command";
 import { NewProjectCommand } from "./commands/new-project/new-project.command";
+import { HelpCommand } from "./commands/help/help.command";
 import { NewProjectOptions } from "./models/command-options/new-project-options/new-project-options";
 import { CommandOptions } from "./models/command-options/command-options";
 
@@ -17,15 +18,20 @@ export const main = function (...args: string[]): number {
     `);
     program
         .version('0.1.0')
-        .option('-n, --new [name]', 'Generate a new project', 'hello-world')
+        .option('-n, --new [name]', 'Generate a new project')
+        .option('-h, --help','Help')
         .parse(process.argv);
     
-    if (program.new) {
+    if (program.new && program.new !== true) {
         const name: string = program.new;
         opts = new NewProjectOptions(name);
         cmd = new NewProjectCommand();
         cmd.run(opts);
+    } else {
+        opts = <any>undefined;
+        cmd = new HelpCommand();
     }
+    cmd.run(opts);
 
     return 0;
 }
