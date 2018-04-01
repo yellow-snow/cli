3ec5d6333d55c99ab3c9663bd4858a1647b3aca1
johnfedoruk <johnny@johnfedoruk.ca>
2018-04-01 00:19:07 -0500

    Linting

--- src/commands/command.ts
+++ src/commands/command.ts
@@ -1,5 +1,5 @@
-import { Observable } from 'rxjs/Observable';
-import { CommandOptions } from 'commander';
+import { CommandOptions } from "commander";
+import { Observable } from "rxjs/Observable";
 
 export abstract class Command {
     private start: Date;
@@ -9,28 +9,26 @@ export abstract class Command {
     public async run(opts: CommandOptions): Promise<void> {
         try {
             await this.beforeScript(opts);
-        } catch(err) {
+        } catch (err) {
             this.beforeScriptError(err);
         }
         try {
             await this.mainScript(opts);
-        }
-        catch(err) {
+        } catch (err) {
             this.afterScriptError(err);
         }
         try {
             await this.afterScript(opts);
-        }
-        catch(err) {
+        } catch (err) {
             this.afterScriptError(err);
         }
     }
     protected abstract async beforeScript(opts: any): Promise<void>;
     protected abstract async mainScript(opts: any): Promise<void>;
     protected abstract async afterScript(opts: any): Promise<void>;
-    protected abstract beforeScriptError(err:Error): never;
-    protected abstract mainScriptError(err:Error): never;
-    protected abstract afterScriptError(err:Error): never;
+    protected abstract beforeScriptError(err: Error): never;
+    protected abstract mainScriptError(err: Error): never;
+    protected abstract afterScriptError(err: Error): never;
     protected get time(): number {
         return (new Date()).getTime() - this.start.getTime();
     }
