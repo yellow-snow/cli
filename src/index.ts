a94396f06ad1fe1fb55d9822ac9ff35c4cfd0448
johnfedoruk <johnny@johnfedoruk.ca>
2018-03-31 19:31:33 -0500

    Added test for --new

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,11 @@ export const main = function (...args: string[]): number {
     \\ V / __|| |  | | / \\  / _|| \\| |/ \\| | | |
      \\ /| _| | |_ | |( o ) \\_ \\| \\\\ ( o ) V V |
      |_||___||___||___\\_/  |__/|_|\\_|\\_/ \\_n_/
-    `)
+    `);
+    program
+        .version('0.1.0')
+        .option('-n, --new [name]', 'Generate a new project')
+        .parse(process.argv);
+    if (program.new) console.log(`Generating a new project ${program.new}`);
     return 0;
 }
