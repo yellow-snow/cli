3430481da9eafdd8650c1b960c9a0f091233d212
johnfedoruk <johnny@johnfedoruk.ca>
2018-03-31 19:30:49 -0500

    Added splash title to main

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
+import * as program from "commander";
+
 export const main = function (...args: string[]): number {
-    console.log("Hello world");
+    console.log(`
+    __ _____  _    _   _    __  _  _  _  _ _ _ 
+    \\ V / __|| |  | | / \\  / _|| \\| |/ \\| | | |
+     \\ /| _| | |_ | |( o ) \\_ \\| \\\\ ( o ) V V |
+     |_||___||___||___\\_/  |__/|_|\\_|\\_/ \\_n_/
+    `)
     return 0;
 }
